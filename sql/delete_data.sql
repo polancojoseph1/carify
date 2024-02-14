d8098cdee271decb93dce4868c0ad2132f16b9a4
Joseph <polanco.joseph1@gmail.com>
2024-02-14 09:08:56 -0500

    added change to only drop if table exists and added sql calls to package.json

--- sql/delete_data.sql
+++ sql/delete_data.sql
@@ -1,20 +1,24 @@
--- Delete all data from the cart_product table
-DELETE FROM cart_product;
+-- Delete all data from the cart_product table if it exists
+DELETE FROM cart_product WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'cart_product');
 
--- Delete all data from the cart table
-DELETE FROM cart;
+-- Delete all data from the cart table if it exists
+DELETE FROM cart WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'cart');
 
--- Delete all data from the payment_account table
-DELETE FROM payment_account;
+-- Delete all data from the payment_account table if it exists
+DELETE FROM payment_account WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'payment_account');
 
--- Delete all data from the payment table
-DELETE FROM payment;
+-- Delete all data from the payment table if it exists
+DELETE FROM payment WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'payment');
 
--- Delete all data from the product table
-DELETE FROM product;
+-- Delete all data from the product table if it exists
+DELETE FROM product WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'product');
 
--- Delete all data from the "user" table
-DELETE FROM "user";
+-- Delete all data from the "user" table if it exists
+DELETE FROM "user" WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'user');
+
+-- Delete all data from the session table if it exists
+DELETE FROM session WHERE EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'session');
+
 
 -- Drop the cart_product table
 DROP TABLE IF EXISTS cart_product CASCADE;
@@ -33,3 +37,6 @@ DROP TABLE IF EXISTS product CASCADE;
 
 -- Drop the "user" table
 DROP TABLE IF EXISTS "user" CASCADE;
+
+-- Drop the "user" table
+DROP TABLE IF EXISTS session CASCADE;
