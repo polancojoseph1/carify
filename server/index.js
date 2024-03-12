4b641fb68b182c282cc7fd22f2540d0b9f918021
Joseph <polanco.joseph1@gmail.com>
2024-03-12 19:33:35 -0400

    cors handler

--- server/index.js
+++ server/index.js
@@ -62,7 +62,6 @@ const createApp = () => {
   // allowing access to the carify react app
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', 'https://carify-liard.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
