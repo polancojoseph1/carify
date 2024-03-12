62c4819183adce1d2269669473496a2071e2049e
Joseph <polanco.joseph1@gmail.com>
2024-03-12 00:54:14 -0400

    bug fix

--- server/api/user.js
+++ server/api/user.js
@@ -4,12 +4,8 @@ const {
   getConnection, 
   getAllUsers,
   getUserById,
-  updateUser,
-  loginUser,
-  signUpUser,
-  guestUser
+  updateUser
 } = require('../db');
-const {generateRandomId} = require('./utils');
 
 getConnection()
 
@@ -41,72 +37,6 @@ router.put('/:id', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-});
-
-router.post('/login', async (req, res, next) => {
-  try {
-    // Login existing user
-    const { email, password } = req.body;
-    const user = await loginUser(email, password);
-    if (!user) {
-      res.status(401).send('Wrong username and/or password');
-    } else {
-      req.login(user, err =>
-        err ? next(err) : res.json({user})
-      );
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/signup', async (req, res, next) => {
-  try {
-    // Create new user
-    const {email, name, password} = req.body;
-    const user = await signUpUser(generateRandomId(), email, name, password);
-
-    req.login(user, err =>
-      err ? next(err) : res.json({user})
-    );
-  } catch (err) {
-    if (err.constraint === 'user_email_key') {
-      res.status(401).send('User already exists');
-    } else {
-      next(err);
-    }
-  }
-});
-
-router.post('/guest', async (req, res, next) => {
-  try {
-    // Create guest user
-    const user = await guestUser(generateRandomId(), generateRandomId())
-
-    req.login(user, err =>
-      err ? next(err) : res.json({message: 'Guest user managed successfully!', user})
-    )
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/logout', (req, res, next) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    req.session.destroy(function(err) {
-      if (err) {
-        return next(err);
-      }
-      res.send({"message": "Successfully logged out!"})
-    });
-  });
-});
-
-router.get('/me', (req, res) => {
-  res.json(req.user);
 });
 
 module.exports = router;
--- server/db/user.js
+++ server/db/user.js
@@ -36,65 +36,8 @@ async function updateUser(id, name, email) {
   return user;
 }
 
-async function loginUser(email, password) {
-  const query = `
-    SELECT * FROM "user"
-    WHERE
-      email = '${email}' AND
-      password = '${password}'
-    LIMIT 1`;
-  const [user] = await process.postgresql.query(query);
-  return user
-}
-
-async function signUpUser(newId, email, name, password) {
-  const query = `
-    INSERT INTO "user" (
-      id,
-      email,
-      name,
-      password
-    )
-    VALUES (
-      ${newId},
-      '${email}', 
-      '${name}',
-      '${password}'
-    )
-    RETURNING *
-    `
-  const [user] = await process.postgresql.query(query);
-  return user
-}
-
-async function guestUser(newId, randomKey) {
-  const query = `
-  INSERT INTO "user" (
-    id,
-    name,
-    email,
-    guest,
-    password
-  )
-  VALUES (
-    ${newId},
-    'guest',
-    '${`guest${randomKey}@email.com`}',
-    ${true},
-    'guest'
-  )
-  LIMIT 1
-  RETURNING *
-  `
-  const [user] = await process.postgresql.query(query);
-  return user
-}
-
 module.exports = {
   getAllUsers,
   getUserById,
-  updateUser,
-  loginUser,
-  signUpUser,
-  guestUser
+  updateUser
 }
